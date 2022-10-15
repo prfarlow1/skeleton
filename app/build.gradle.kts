6c5de12a9ef01dab601530c18826f42a39b5fe6e
Peter Farlow <6036075+prfarlow1@users.noreply.github.com>
2022-10-15 18:45:34 -0400

    Re-arrange deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,26 +47,25 @@ dependencies {
 
     implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
+
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
+    implementation("androidx.activity:activity-compose:1.6.0")
 
     implementation("androidx.compose.material3:material3:1.0.0-rc01")
-
-    // Android Studio Preview support
     implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.2.1")
 
-    // Optional - Integration with activities
-    implementation("androidx.activity:activity-compose:1.6.0")
-    // Optional - Integration with ViewModels
-
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     implementation("androidx.lifecycle:lifecycle-process:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.5.1")
+
+    implementation("androidx.startup:startup-runtime:1.1.1")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
-    implementation("androidx.startup:startup-runtime:1.1.1")
 
     implementation("org.tinylog:tinylog-impl:2.5.0")
     implementation("org.tinylog:tinylog-api-kotlin:2.5.0")
