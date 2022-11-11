9c109e1dfc6cee433c68eb2e16ebe76e15442e29
Peter Farlow <6036075+prfarlow1@users.noreply.github.com>
2022-11-10 23:53:49 -0500

    Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -50,14 +51,14 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
 
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
 
-    implementation("androidx.compose.material3:material3:1.0.0-rc01")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.2.1")
+    implementation("androidx.compose.material3:material3:1.0.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.1")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     implementation("androidx.lifecycle:lifecycle-process:2.5.1")
